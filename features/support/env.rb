596a60165593429cecdc911b462f59514cdce76b
Stanislaw Malinowski <smmalinowski1@sheffield.ac.uk>
2020-03-21 15:15:12 +0000

    further testing

---
+++ features/support/env.rb
@@ -0,0 +1,22 @@
+require 'capybara'
+require 'cucumber'
+require 'rspec'
+require 'simplecov'
+require 'capybara/cucumber'
+
+#is that file extension alright? 
+require_relative '../../startServer.rb'
+
+ENV['RACK_ENV'] = 'test'
+
+Capybara.app = Sinatra::Application
+
+class Sinatra::ApplicationWorld
+    include RSpec::Expectations
+    include RSpec::Matchers
+    include Capybara::DSl
+end
+
+World do
+    Sinatra::ApplicationWorld.new
+end
--- testingController.rb
+++ testingController.rb
@@ -3,35 +3,45 @@ require 'sinatra/reloader'
 require 'sqlite3'
 require_relative 'controller'
 
-require 'capybara'
-require 'cucumber'
-require 'rspec'
-require 'simplecov'
+require 'minitest/autorun'
 
-require "sqlite3"
-
-class BookmarkDB
-    
-    def initialize
-        @db = SQLite3::Database.new "./database.db"
+class ControllerComparison < Minitest::Test
+    #if the database exists
+    def initialize_test
+        assert_equals true, @db
     end
 
-    def check_account_exists(username)
-        # If get_account_id returns something, there's an account
-        # Else return false because no account
-        if get_account_id(username)
-            return true
-        else
-            return false
-        end
+#check_account_exists
+    #with positive value 
+    def check_account_exists_test_pos
+        assert_equals check_account_exists "Jake1"
     end
     
+    #with negative value 
+    def check_account_exists_test_neg
+        assert_equals check_account_exists "Jake3"
+    end
+    
+    #with empty string 
+    def check_account_exists_test_empty
+        assert_equals check_account_exists ""
+    end
+    
+    #with nill value  
+    def check_account_exists_test_nil
+        assert_equals check_account_exists nil
+    end
+
+#get_account_id
+    #original function
     def get_account_id(username)
         statement = "SELECT user_id FROM users WHERE username = ?"
         retStatement = @db.execute statement, username
         return retStatement[0]
     end
-    
+   
+#try login
+    #original function
     def try_login(username, password)
         statement = "SELECT user_id from users WHERE username = ? AND password = ?"
         retStatement = @db.execute statement, username, password
@@ -40,13 +50,19 @@ class BookmarkDB
         end
             return false
     end
-    
+
+
+#get_username_email
+    #original function
     def get_username_email(email)
         statement = "SELECT username FROM users WHERE email = ?"
         retStatement = @db.execute statement, email
         return retStatement[0]
     end
-    
+ 
+
+#create_account
+    #original function
     def create_account(username, password, first_name, last_name, email) # Doesn't need account type, seperate function to update
         if not get_account_id(username)
             if not get_username_email(email) ## unique username and email
@@ -60,22 +76,32 @@ class BookmarkDB
         return "fail-username"
     end
     
+    
+    #successful case
+    #fail email case
+    #fail username
+    #empty strings
+    #null values
+    #too long values
+    #different characters
+
+#add_security_questions
+    #not yet implemented
     def add_security_questions(username, sec_question, sec_answer)
         
     end
-    
+
+#get_login_attempts
+    #original function
     def get_login_attempts(username)
         statement = "SELECT login_attempts FROM users WHERE username = ?"
         retStatement = @db.execute statement, username
         return retStatement[0]
     end
-    
-    
-    #display_users has no return value
+  
+#display_users 
+    #has no return value
     def display_users_test
         puts "display_users has no return value"
     end
-    
-    
-    
 end
