b2fa7a58d40fc004ee6ef0ca0b85265a810a49ce
TheDarthMole <27959011+TheDarthMole@users.noreply.github.com>
2020-03-21 15:18:12 +0000

    Added hashing and started password changing

--- startServer.rb
+++ startServer.rb
@@ -1,13 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'openssl'
 require_relative 'controller'
 
 set :bind, '0.0.0.0'
 # set :public_folder, 'public'
 
 configure do
-    @@db = BookmarkDB.new
+    @@database = BookmarkDB.new
     enable :sessions
 end
 
@@ -18,7 +19,7 @@ end
 post "/" do
     session[:user] = params[:username]
     session[:pass] = params[:password]
-    if @@db.try_login(session[:user], session[:pass])
+    if @@database.try_login(session[:user], session[:pass])
         redirect "/loggedin"
     else
         redirect "/"
@@ -43,13 +44,17 @@ post "/register" do
 end
 
 get "/loggedin" do
-    if not @@db.try_login(session[:user], session[:pass])
+    if not @@database.try_login(session[:user], session[:pass])
         redirect "/"
     else
-        @bookmarks = @@db.get_all_bookmarks
+        @bookmarks = @@database.get_all_bookmarks
         erb :bookmarks
     end
 end
 
 post "/loggedin" do
+end
+
+get "/change-password" do
+    erb :changePassword
 end
