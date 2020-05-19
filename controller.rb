1d85d5d432f3130920eb4e3ec5ba6f106a5f9b39
Nicholas Ruffles <nruffles1@sheffield.ac.uk>
2020-05-19 21:04:10 +0000

    FINALLY added reporting and deleting bookmarks!

--- controller.rb
+++ controller.rb
@@ -328,7 +328,7 @@ class BookmarkDB
         results = results.to_i
         i_min = (page-1)*results
         search = '%'+term+'%'
-        retStatment = "SELECT distinct bookmarks.bookmark_id,bookmarks.bookmark_name,bookmarks.url,bookmarks.creation_time FROM bookmark_tags , bookmarks, tags WHERE bookmarks.bookmark_name LIKE ? OR (tags.name LIKE ? AND tags.tag_id=bookmark_tags.tag_ID AND bookmark_tags.bookmark_ID=bookmarks.bookmark_id) OR bookmarks.url LIKE ? LIMIT ?,?"
+        retStatment = "SELECT distinct bookmarks.bookmark_id,bookmarks.bookmark_name,bookmarks.url,bookmarks.creation_time FROM bookmark_tags , bookmarks, tags WHERE (bookmarks.bookmark_name LIKE ? OR (tags.name LIKE ? AND tags.tag_id=bookmark_tags.tag_ID AND bookmark_tags.bookmark_ID=bookmarks.bookmark_id) OR bookmarks.url LIKE ?) AND bookmarks.enabled=1  LIMIT ?,?"
         sql = @db.execute retStatment,search,search,search,i_min,results
         #Adds the tags into results
         i_max = sql.length
